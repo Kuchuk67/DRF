33dd558b3e503581729bb1c8042529954765735c
Serg Kuchukov Office <kuchukov.sergey@gmail.ru>
2025-03-18 16:57:51 +0300

    Добавьте новую модель в приложение users

--- README.md
+++ README.md
@@ -19,5 +19,14 @@
 
 
 
+python manage.py loaddata data_test\Course_fixture.json
+python manage.py loaddata data_test\Lesson_fixture.json
+python manage.py loaddata data_test\User_fixture.json
+python manage.py loaddata data_test\Pay_fixture.json
+
+
 python  -Xutf8 manage.py dumpdata education.Course --output data_test\Course_fixture.json --indent 4
 python  -Xutf8 manage.py dumpdata education.Lesson --output data_test\Lesson_fixture.json --indent 4
+python  -Xutf8 manage.py dumpdata users.CustomUser --output data_test\User_fixture.json --indent 4
+python  -Xutf8 manage.py dumpdata users.Pay --output data_test\Pay_fixture.json --indent 4
+
--- users/models.py
+++ users/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator
+from education.models import Course, Lesson
+
 
 # Create your models here.
 class CustomUser(AbstractUser):
@@ -28,3 +31,50 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+
+STATUS_CHOICES = [
+    ("cash", "Наличные"),
+    ("transfer", "перевод на счет"),
+]
+
+class Pay(models.Model):
+    user = models.ForeignKey(CustomUser,
+                            null=False,
+                            on_delete=models.CASCADE,
+                            related_name="paid_lesson"
+                            )
+
+    data_at = models.DateTimeField(auto_now_add=True,
+                                   verbose_name="дата оплаты"
+                                   )
+
+    paid_course = models.ForeignKey(Course,
+                                    null=True,
+                                    on_delete=models.CASCADE,
+                                    related_name="paid_course"
+                                    )
+
+    paid_lesson = models.ForeignKey(Lesson,
+                                    null=True,
+                                    on_delete=models.CASCADE,
+                                    related_name="paid_lesson"
+                                    )
+
+    summa_pay = models.IntegerField(null=False,
+                                 blank=False,
+                                 default=1,
+                                 validators=[MinValueValidator(0)],
+                                 verbose_name="сумма оплаты")
+
+    payment_method = models.CharField(max_length=10,
+                                      choices=STATUS_CHOICES,
+                                      default="cash",
+                                      verbose_name="способ оплаты")
+    class Meta():
+        verbose_name = "Оплата"
+        verbose_name_plural = "Оплаты"
+        ordering = ["-data_at"]
+
+    def __str__(self):
+        return self.data_at
+
