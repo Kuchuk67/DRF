82023d1271ba26cfcf43f0f5874841693489321a
Serg_Nout <kuchukov.sergey@gmail.com>
2025-03-18 23:07:46 +0300

    поле вывода уроков

--- education/serializer.py
+++ education/serializer.py
@@ -10,13 +10,19 @@ class CourseSerializer(ModelSerializer):
     def get_count_lessons(self, course):
         return Lesson.objects.filter(course=course.pk).count()
 
+    lessons_of_course = SerializerMethodField()
+    def get_lessons_of_course(self, course):
+        return [lesson.name_lesson for lesson in Lesson.objects.filter(course=course).order_by('id')]
+
+
     class Meta:
         model = models.Course
         fields =  ["id",
                 "course_name",
                 "description",
                 "image",
-                "count_lessons"
+                "count_lessons",
+                "lessons_of_course"
                 ]
 
 class LessonsSerializer(ModelSerializer):
